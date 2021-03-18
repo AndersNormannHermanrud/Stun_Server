3b182a8cdc1299d639385f672209754522624d8e
kazil <andmar82@gmail.com>
2021-03-18 13:30:51 +0100

    constructor

--- Src/stun.js
+++ Src/stun.js
@@ -1,4 +1,12 @@
 const dgram = require("dgram");
 class stun {
-    console.log("hello ass")
+    constructor ({port = 3478, udp4 = true, udp6 = true, log = console.log, err = console.error, sw = true} = {}) {
+        this.udp4 = udp4;
+        this.udp6 = udp6;
+        this.port = port;
+        this.log = log;
+        this.err = err;
+        this.sw = sw;
+        this.socket = null;
+    }
 }
