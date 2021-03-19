49243c94d05b8a8edce2de6718b5ae2a1c57e4a4
kazil <andmar82@gmail.com>
2021-03-19 15:23:56 +0100

    minifeil

--- Src/message.js
+++ Src/message.js
@@ -87,5 +87,6 @@ function hexStringToByte(str) {
     for (var i = 0, len = str.length; i < len; i+=2) {
         a.push(parseInt(str.substr(i,2),16));
     }
+
     return new Uint8Array(a);
-  }
+}
