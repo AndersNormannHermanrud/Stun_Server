ab571b83d35a49c4f4601c1148a71c8f7c4a1a43
kazil <andmar82@gmail.com>
2021-03-19 15:31:57 +0100

    Stun

--- Src/stun.js
+++ Src/stun.js
@@ -2,9 +2,20 @@ const crypto = require('crypto');
 const util = require('util');
 const Message = require('./Message');
 const net = require('dgram')
-socket = net.createSocket('udp4');
+const socket = net.createSocket('udp4');
 
-function Server(options) {
+socket.on('message', (message, rinfo)=>{
+    const newMessage = Message.decode(message);
+    console.log('Från klient : ' , message);
+    console.log(rinfo);
+    const rMessage = Message.encodeMessage(message, newMessage, rinfo.address, rinfo.port);
+    const client = net.createSocket('udp4');
+    client.send(rMessage, rinfo.port, rinfo.address);
+    console.log(rMessage);
+})
+
+
+/* function Server(options) {
     options = options || {};
 
     this.type = options.type || Server.TYPE.TLS;
@@ -23,4 +34,4 @@ Server.TYPE = {
     TLS: 0,
     TCP: 0,
     UDP: 1
-};
+}; */
