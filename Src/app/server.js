82b17b37499ed432d0d85fc2396ad375770b9c36
Anders <andernh@stud.ntnu.no>
2021-03-20 15:25:40 +0100

    Added fake rest to server.js

--- Src/app/server.js
+++ Src/app/server.js
@@ -9,8 +9,14 @@ let indexFile;
 
 const requestListener = function (req, res) {
     res.setHeader("Content-Type", "text/html");
-    res.writeHead(200);
-    res.end(indexFile);
+    switch (req.url) {
+        case "planning_poker":
+            break
+        default:
+            res.writeHead(200);
+            res.end(indexFile);
+            break
+    }
 };
 
 const server = http.createServer(requestListener);
